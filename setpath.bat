e02e19550e2b40ce28a45fe5f2f8e721c922d104
loserStar <362527240@qq.com>
2017-02-08 21:08:02 +0800

    update setpath.bat

--- setpath.bat
+++ setpath.bat
@@ -4,10 +4,10 @@ chcp 936
 color a
 echo ----------running maven start----------------------
 rem @setlocal
-set MAVEN_HOME=E:\myIDE\maven-3.3.9\bin
 set JAVA_HOME=e:\jdk1.8
 set classpath=.;.\classes\;.\target\test-classes\;.\target\classes\;%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\tools.jar;
 set LANG=zh_CN.UTF8
 set path=.;%MAVEN_HOME%;%JAVA_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
+set MAVEN_HOME=E:\myIDE\maven-3.3.9\bin
 call mvn -v
 echo ------------end--------------
