8fc152e0c267f157558a03cc3e8bc825afdad8b4
loserStar <362527240@qq.com>
2018-03-09 17:24:38 +0800

    练习io

--- src/main/java/com/loserstar/utils/freemarker/LoserStarFreemarkerUtil.java
+++ src/main/java/com/loserstar/utils/freemarker/LoserStarFreemarkerUtil.java
@@ -1,8 +1,17 @@
 package com.loserstar.utils.freemarker;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,7 +22,7 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateNotFoundException;
 
-public class FreemarkerUtil {
+public class LoserStarFreemarkerUtil {
 /*	Freemarker提供了3种加载模板目录的方法。 它使用Configuration类加载模板。
 	三种方法分别是：
 	public void setClassForTemplateLoading(Class clazz, String pathPrefix);
@@ -33,7 +42,9 @@ public class FreemarkerUtil {
 */
 	
 
-	public static void main(String[] args) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+
+
+	public static void main(String[] args) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, TemplateException {
 		Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
 		cfg.setDefaultEncoding("UTF-8");
 		/**
@@ -44,12 +55,28 @@ public class FreemarkerUtil {
 			file:/E:/loserStarWorkSpace/loserStarUtils/target/classes/
 			所以第二个参数得加上包路径，但是mvn编译并不会把src目录下的除*.java之外的文件打包过去，所以我们的ftl文件得放到resources目录的同包名目录下
 		 */
-		cfg.setClassForTemplateLoading(FreemarkerUtil.class, "/com/loserstar/utils/freemarker");
-		System.out.println(FreemarkerUtil.class.getResource("/"));
+		cfg.setClassForTemplateLoading(LoserStarFreemarkerUtil.class, "/com/loserstar/utils/freemarker");
+		System.out.println(LoserStarFreemarkerUtil.class.getResource("/"));
 //		cfg.setDirectoryForTemplateLoading(new File("E:\\loserStarWorkSpace\\loserStarUtils\\src\\main\\java\\com\\loserstar\\utils\\freemarker"));
-		Template temp =cfg.getTemplate("test.ftl");
 		Map<String, Object> map = new HashMap<>();
-		map.put("data", "loserStar!");
-		temp.process(map, new OutputStreamWriter(System.out));
+		try {
+			Template temp =cfg.getTemplate("test.ftl");
+			System.out.println("读一个字符");
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+			char c = (char)bufferedReader.read();
+			char c2= (char)bufferedReader.read();
+			map.put("data", "loserStar freemakrer!!!!!"+c+c2);
+			StringWriter stringWriter = new StringWriter();
+			temp.process(map,stringWriter );
+			System.out.println(stringWriter.toString());
+			String filePath = "c://loserStarFreemarkerTemplate.txt";
+			File file = new File(filePath);
+			FileWriter fileWriter = new FileWriter(file);
+			fileWriter.write(stringWriter.toString());
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
