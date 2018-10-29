f77ba1156e4372657f76bad71838907b059ae085
loserStar <362527240@qq.com>
2018-10-29 19:14:40 +0800

    更新freemarker版本，提供使用freemaker的简化方法

--- src/main/java/com/loserstar/utils/freemarker/LoserStarFreemarkerUtil.java
+++ src/main/java/com/loserstar/utils/freemarker/LoserStarFreemarkerUtil.java
@@ -1,12 +1,11 @@
 package com.loserstar.utils.freemarker;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import com.loserstar.utils.file.LoserStarFileUtil;
 
@@ -17,6 +16,12 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateNotFoundException;
 
+/**
+ * 
+ * author: loserStar
+ * date: 2018年10月29日下午7:12:59
+ * remarks: freemarker工具类
+ */
 public class LoserStarFreemarkerUtil {
 /*	Freemarker提供了3种加载模板目录的方法。 它使用Configuration类加载模板。
 	三种方法分别是：
@@ -36,10 +41,33 @@ public class LoserStarFreemarkerUtil {
 	configuration.getTemplate("framemaker.ftl"); //framemaker.ftl为要装载的模板
 */
 	
+	/**
+	 * 执行模板，输出运行后的字符串
+	 * web环境下执行使用的方法
+	 * @param request 请求对象
+	 * @param templateDir 模板所在路径（传入web工程根目录下的相对路径，不带文件名的，文件名要单独传）
+	 * @param templateName 模板文件名
+	 * @param data 填充的数据
+	 * @return
+	 * @throws TemplateNotFoundException
+	 * @throws MalformedTemplateNameException
+	 * @throws ParseException
+	 * @throws IOException
+	 * @throws TemplateException
+	 */
+	public static String runForWeb(HttpServletRequest request,String templateDir,String templateName,Object data) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setServletContextForTemplateLoading(request.getSession().getServletContext(),templateDir );
+		Template temp = cfg.getTemplate(templateName); 
+		StringWriter stringWriter = new StringWriter();
+		temp.process(data,stringWriter);
+		String reusltStr = stringWriter.toString();
+		return reusltStr;
+	}
 
 
-
-	public static void main(String[] args) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, TemplateException {
+	public static void main(String[] args) throws Exception {
 		Configuration cfg = new Configuration(Configuration.VERSION_2_3_23);
 		cfg.setDefaultEncoding("UTF-8");
 		/**
