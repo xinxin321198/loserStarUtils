1830226446c79ebb098b27c4153292bfd9252af2
loserStar <362527240@qq.com>
2018-11-20 17:51:51 +0800

    增加使用多数据源的判断，可以兼容老代码 如要要使用多数据源，继承自BaseService的子类，得实现有参数的那个构造方法，并且实例化时候传入jfinal配置中的要访问的数据源

--- src/main/java/com/loserstar/utils/db/jfinal/base/imp/BaseService.java
+++ src/main/java/com/loserstar/utils/db/jfinal/base/imp/BaseService.java
@@ -11,8 +11,8 @@ import com.loserstar.utils.idgen.SnowflakeIdWorker;
 /**
  * 
  * author: loserStar
- * date: 2018年11月15日下午10:19:06
- * remarks:基础server，加入获取单挑数据，数据库的判断
+ * date: 2018年11月19日下午12:07:26
+ * remarks:基础server，加入多数据源的判断
  */
 public  abstract class BaseService {
 	public enum DBType{
@@ -46,6 +46,8 @@ public  abstract class BaseService {
 	 */
 	protected abstract String getPrimaryKey();
 	
+	protected String dataSourceName;//jfinal使用多数据源时候指定数据源名称
+	
 	/**
 	 * 返回该表软删除使用的字段，查询列表的方法会自动过滤该字段值(如果未指定该字段(null或者空字符串)，则认为该表不具有软删除功能)
 	 * @return
@@ -62,6 +64,20 @@ public  abstract class BaseService {
 			if (getPrimaryKey()==null||getPrimaryKey().equals("")) {
 				throw new Exception(this.getClass().getName()+" 中的getPrimaryKey()方法没有指定表的主键名称");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public BaseService(String dataSourceName) {
+		try {
+			if (getTableName()==null||getTableName().equals("")) {
+				throw new Exception(this.getClass().getName()+" 中的getTableName()方法没有指定表名称");
+			}
+			if (getPrimaryKey()==null||getPrimaryKey().equals("")) {
+				throw new Exception(this.getClass().getName()+" 中的getPrimaryKey()方法没有指定表的主键名称");
+			}
+			this.dataSourceName = dataSourceName;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -94,6 +110,17 @@ public  abstract class BaseService {
 			return true;
 		}
 	}
+	/**
+	 * 检测是否指定jfinal的数据源
+	 * @return 指定返回true，未指定返回false
+	 */
+	private boolean CheckDataSourceName() {
+		if (this.dataSourceName==null||this.dataSourceName.equals("")) {
+			return false;
+		}else {
+			return true;
+		}
+	}
 	
 	/**
 	 * 添加softDelField的过滤条件
@@ -111,12 +138,13 @@ public  abstract class BaseService {
 	}
 	
 	/**
-	 * 根据sql得到单调数据
+	 * 根据sql得到单调数据（效率慢，因为是取所有数据出来再取第一条）
+	 * @see com.loserstar.utils.db.jfinal.base.imp.BaseService.getFirstList(WhereHelper, DBType)
 	 * @param sql
 	 * @return
 	 */
 	public Record get(String sql) {
-		List<Record> list = Db.find(sql);
+		List<Record> list = CheckDataSourceName()?Db.use(this.dataSourceName).find(sql):Db.find(sql);
 		if (list!=null&&list.size()>0) {
 			return list.get(0);
 		}else {
@@ -125,12 +153,13 @@ public  abstract class BaseService {
 	}
 	
 	/**
-	 * 根据sql得到单调数据(参数化查询方式)
+	 * 根据sql得到单调数据(参数化查询方式，效率慢，因为是取所有数据出来再取第一条)
+	 * @see com.loserstar.utils.db.jfinal.base.imp.BaseService.getFirstList(WhereHelper, DBType)
 	 * @param sql
 	 * @return
 	 */
 	public Record get(String sql,Object... paras) {
-		List<Record> list = Db.find(sql,paras);
+		List<Record> list = CheckDataSourceName()?Db.use(this.dataSourceName).find(sql, paras):Db.find(sql,paras);
 		if (list!=null&&list.size()>0) {
 			return list.get(0);
 		}else {
@@ -157,7 +186,7 @@ public  abstract class BaseService {
 	 */
 	public boolean updateFieldIsNull(String fieldName,String primaryId) {
 		String sql = "UPDATE "+getTableName()+" SET "+fieldName+" = NULL WHERE "+getPrimaryKey()+"='"+primaryId+"'";
-		int row = Db.update(sql);
+		int row = CheckDataSourceName()?Db.use(this.dataSourceName).update(sql):Db.update(sql);
 		if (row<1) {
 			return false;
 		}
@@ -165,7 +194,7 @@ public  abstract class BaseService {
 	}
 	public boolean updateFieldIsNull(String fieldName,long primaryId) {
 		String sql = "UPDATE "+getTableName()+" SET "+fieldName+" = NULL WHERE "+getPrimaryKey()+"="+primaryId+"";
-		int row = Db.update(sql);
+		int row = CheckDataSourceName()?Db.use(this.dataSourceName).update(sql):Db.update(sql);
 		if (row<1) {
 			return false;
 		}
@@ -192,7 +221,7 @@ public  abstract class BaseService {
 	 */
 	public List<Record> getList_notSoftDel(WhereHelper whereHelper){
 		String sql ="select * from "+getTableName()+CheckWhereHelper(whereHelper);
-		return Db.find(sql);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).find(sql):Db.find(sql);
 	}
 	
 	/**
@@ -233,7 +262,7 @@ public  abstract class BaseService {
 			sql+=joinHelper.toString();
 		}
 		sql+=CheckWhereHelper(whereHelper);
-		return Db.find(sql);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).find(sql):Db.find(sql);
 	}
 	
 	/**
@@ -290,7 +319,7 @@ public  abstract class BaseService {
 	 */
 	public Page<Record> getListPage_notSoftDel(int pageNumber,int pageSize,WhereHelper whereHelper){
 		String sqlExceptSelect = "from "+getTableName()+CheckWhereHelper(whereHelper);
-		return Db.paginate(pageNumber, pageSize, "select *",sqlExceptSelect);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).paginate(pageNumber, pageSize, "select *",sqlExceptSelect):Db.paginate(pageNumber, pageSize, "select *",sqlExceptSelect);
 	}
 	
 	/**
@@ -299,7 +328,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public Record getById(String id) {
-		return Db.findById(getTableName(), getPrimaryKey(), id);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).findById(getTableName(),getPrimaryKey(),id):Db.findById(getTableName(), getPrimaryKey(), id);
 	}
 	
 	/**
@@ -308,7 +337,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public Record getById(long id) {
-		return Db.findById(getTableName(), getPrimaryKey(), id);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).findById(getTableName(), getPrimaryKey(), id):Db.findById(getTableName(), getPrimaryKey(), id);
 	}
 	
 	/**
@@ -320,9 +349,9 @@ public  abstract class BaseService {
 		boolean flag = false;
 		if (record.getStr(getPrimaryKey())==null||record.getStr(getPrimaryKey()).equals("")) {
 			record.set(getPrimaryKey(), UUID.randomUUID().toString().replaceAll("-", ""));
-			flag = Db.save(getTableName(),getPrimaryKey(), record);
+			flag = CheckDataSourceName()?Db.use(this.dataSourceName).save(getTableName(), getPrimaryKey(), record):Db.save(getTableName(),getPrimaryKey(), record);
 		}else {
-			flag = Db.update(getTableName(),getPrimaryKey(), record);
+			flag = CheckDataSourceName()?Db.use(this.dataSourceName).update(getTableName(),getPrimaryKey(), record):Db.update(getTableName(),getPrimaryKey(), record);
 		}
 		return flag;
 	}
@@ -336,9 +365,9 @@ public  abstract class BaseService {
 		boolean flag = false;
 		if (record.getStr(getPrimaryKey())==null||record.getStr(getPrimaryKey()).equals("")) {
 			record.set(getPrimaryKey(), SnowflakeIdWorker.FakeGuid());
-			flag = Db.save(getTableName(),getPrimaryKey(), record);
+			flag = CheckDataSourceName()?Db.use(this.dataSourceName).save(getTableName(),getPrimaryKey(), record):Db.save(getTableName(),getPrimaryKey(), record);
 		}else {
-			flag = Db.update(getTableName(),getPrimaryKey(), record);
+			flag = CheckDataSourceName()?Db.use(this.dataSourceName).update(getTableName(),getPrimaryKey(), record):Db.update(getTableName(),getPrimaryKey(), record);
 		}
 		return flag;
 	}
@@ -349,7 +378,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public boolean insert(Record record) {
-		return Db.save(getTableName(),getPrimaryKey(), record);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).save(getTableName(),getPrimaryKey(), record):Db.save(getTableName(),getPrimaryKey(), record);
 	}
 	
 	/**
@@ -358,7 +387,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public boolean update(Record record) {
-		return Db.update(getTableName(), getPrimaryKey(),record);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).update(getTableName(), getPrimaryKey(),record):Db.update(getTableName(), getPrimaryKey(),record);
 	}
 	
 	/**
@@ -366,7 +395,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public int deleteAll() {
-		return Db.delete("DELETE FROM "+getTableName());
+		return CheckDataSourceName()?Db.use(this.dataSourceName).delete("DELETE FROM "+getTableName()):Db.delete("DELETE FROM "+getTableName());
 	}
 	
 	/**
@@ -375,7 +404,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public int deleteByWhere(WhereHelper whereHelper) {
-		return Db.delete("DELETE FROM "+getTableName()+CheckWhereHelper(whereHelper));
+		return CheckDataSourceName()?Db.use(this.dataSourceName).delete("DELETE FROM "+getTableName()+CheckWhereHelper(whereHelper)):Db.delete("DELETE FROM "+getTableName()+CheckWhereHelper(whereHelper));
 	}
 	
 	/**
@@ -384,7 +413,7 @@ public  abstract class BaseService {
 	 * @return
 	 */
 	public boolean deleteById(String id) {
-		return Db.deleteById(getTableName(),getPrimaryKey(), id);
+		return CheckDataSourceName()?Db.use(this.dataSourceName).deleteById(getTableName(),getPrimaryKey(), id):Db.deleteById(getTableName(),getPrimaryKey(), id);
 	}
 	
 	/**
@@ -401,7 +430,7 @@ public  abstract class BaseService {
 		Record record = new Record();
 		record.set(getPrimaryKey(), id);
 		record.set(getSoftDelField(), DEL);
-		flag = Db.update(getTableName(), getPrimaryKey(), record);
+		flag = CheckDataSourceName()?Db.use(this.dataSourceName).update(getTableName(), getPrimaryKey(), record):Db.update(getTableName(), getPrimaryKey(), record);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -414,7 +443,7 @@ public  abstract class BaseService {
 	 * @return 返回每条sql影响的行数
 	 */
 	public int[] batchInsert(List<Record> list) {
-		return Db.batchSave(getTableName(), list, list.size());
+		return CheckDataSourceName()?Db.use(this.dataSourceName).batchSave(getTableName(), list, list.size()):Db.batchSave(getTableName(), list, list.size());
 	}
 	
 	/**
@@ -423,7 +452,7 @@ public  abstract class BaseService {
 	 * @return 返回每条sql影响的行数
 	 */
 	public int[] batchUpdate(List<Record> list) {
-		return Db.batchUpdate(getTableName(), list, list.size());
+		return CheckDataSourceName()?Db.use(this.dataSourceName).batchUpdate(getTableName(), list, list.size()):Db.batchUpdate(getTableName(), list, list.size());
 	}
 	/**
 	 * 批量保存，根据flag标记来判断删除还是新增修改(c新增u修改r读取d删除)u在无id的情况下为新增
