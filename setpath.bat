180cfa07a1a9d32774320f3d136fd0d458e511d5
loserStar <362527240@qq.com>
2019-08-12 11:06:19 +0800

    开始使用版本号管理 1.0，支持jdk1.6，maven最高也只能支持到3.2

--- setpath.bat
+++ setpath.bat
@@ -5,7 +5,7 @@ color a
 echo ----------running maven start----------------------
 rem @setlocal
 set JAVA_HOME=e:\jdk1.6
-set MAVEN_HOME=E:\maven2.5\bin
+set MAVEN_HOME=E:\maven3.2\bin
 set classpath=.;.\classes\;.\target\test-classes\;.\target\classes\;%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\tools.jar;
 set LANG=zh_CN.UTF8
 set path=.;%MAVEN_HOME%;%JAVA_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
