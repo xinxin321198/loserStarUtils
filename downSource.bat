7b76f3e1f57e425b2fc205ac74422b1ae4abdc69
xinxin321198 <362527240@qq.com>
2020-08-07 12:09:15 +0800

    加个下载源码的配置和批处理命令

---
+++ downSource.bat
@@ -0,0 +1,3 @@
+@call setpath.bat
+mvn mvn dependency:sources
+pause
--- setpath.bat
+++ setpath.bat
@@ -5,7 +5,7 @@ color a
 echo ----------running maven start----------------------
 rem @setlocal
 set JAVA_HOME=D:\development\jdk1.8
-set MAVEN_HOME=D:\development\maven3.2\bin
+set MAVEN_HOME=D:\development\maven3.2
 set classpath=.;.\classes\;.\target\test-classes\;.\target\classes\;%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\tools.jar;
 set LANG=zh_CN.UTF8
 set path=.;%MAVEN_HOME%\bin;%JAVA_HOME%\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem
